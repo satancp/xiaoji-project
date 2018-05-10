5a7a27ab35d1b5fa1d485de3294ef92894955eb3
Chris <zx603852402@gmail.com>
2018-05-10 16:58:38 +0800

    update

--- app/controller/home/homeApi.js
+++ app/controller/home/homeApi.js
@@ -24,7 +24,7 @@ module.exports = app => {
         async listStar() {
             const { ctx } = this;
             const query1 =
-                'SELECT s.id, s.index, s.resource_id, r.name, r.desc, r.preview_image, r.category_id, c.display_name as category ' +
+                'SELECT s.id, s.index, s.resource_id, r.id as resource_id, r.name, r.desc, r.preview_image, r.category_id, c.display_name as category ' +
                 'FROM stars AS s INNER JOIN resources AS r ON r.id = s.resource_id INNER JOIN categories AS c ON c.id = r.category_id';
             let results = await app.mysql.query(query1);
             this.success(results);
