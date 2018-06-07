2863f2362941937c814ed2e171b620ab9e0ca43f
Chris <zx603852402@gmail.com>
2018-06-07 19:41:56 +0800

    add translation

--- app/middleware/errorHandler.js
+++ app/middleware/errorHandler.js
@@ -1,13 +1,17 @@
-'use strict';
-const { generalFailure } = require('../utils/tools');
+'use strict'
+const {generalFailure} = require('../utils/tools')
+const translation = require('../exception/codeTranslation')
 
 module.exports = options => {
     return async function handler(ctx, next) {
         try {
-            await next();
+            await next()
         } catch (e) {
-            console.log(e);
-            ctx.body = generalFailure(e);
+            if (e.code && e.msg) {
+                const locale = ctx.get('locale') ? ctx.get('locale') : 'en'
+                e.msg = translation[e.code][locale]
+            }
+            ctx.body = generalFailure(e)
         }
-    };
-};
+    }
+}
